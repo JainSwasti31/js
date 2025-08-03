2dbb3a9d7350fff60bf7b860b401a3100cf2f358
JainSwasti31 <148178401+JainSwasti31@users.noreply.github.com>
2025-08-03 10:16:03 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,21 @@
+"use strict"; //treat all js code as newer version
+//alert(3+3)// we are using node.js, not browser
+console.log(3+3) //code readability should be high
+console.log("Swasti");
+let age=19
+
+/*number => 2 to power 53/52 (range)
+  bigint => for larger values
+  string => ""
+  boolean => true/false
+  null => standalone value
+  undefined => when no value is assigned to the variable
+  symbol => uniqueness
+*/
+
+//object
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
+console.log(typeof "Swasti")
+console.log(typeof age);
---
+++ 01_basics/variables.js
@@ -0,0 +1,11 @@
+const id=259
+let name="Swasti"
+var branch="cse"
+city="ghaziabad"
+let college
+// if we use const keyword, then the value of that variable cannot be changed.
+/*
+prefer to use const and let instead of var 
+because of issue in block scope and functional scope.
+*/
+console.table([id, name, branch, city, college])
