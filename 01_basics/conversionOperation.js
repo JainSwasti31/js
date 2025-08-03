e712ea4957d7e97229144eb3e63b06ed86c78c4a
JainSwasti31 <148178401+JainSwasti31@users.noreply.github.com>
2025-08-03 10:32:30 +0000

    conversionOperation

---
+++ 01_basics/conversionOperation.js
@@ -0,0 +1,23 @@
+let age="33"
+let score="33bac"
+let valueInNumber=Number(age);
+let valInNumber=Number(score);
+
+console.log(typeof(age));
+console.log(typeof age);
+console.log(typeof valueInNumber);
+console.log(valueInNumber)
+
+console.log(typeof valInNumber);
+console.log(valInNumber)
+
+let booleanIsLoggedIn=Boolean(age);
+console.log(booleanIsLoggedIn)
+
+//1=> true; 0=>false;
+//"" => false;
+//"swasti"=>true
+
+let somenumber=19
+let stringnumber=String(somenumber)
+console.log(stringnumber);
