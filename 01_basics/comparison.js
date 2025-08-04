824077054e48d4b52939f8c3ca2c78638594d10a
JainSwasti31 <148178401+JainSwasti31@users.noreply.github.com>
2025-08-04 09:11:00 +0000

    comparison

---
+++ 01_basics/comparison.js
@@ -0,0 +1,11 @@
+console.log(null>0)
+console.log(null == 0)
+console.log(null >= 0)
+
+console.log(undefined>0)
+console.log(undefined == 0)
+console.log(undefined >= 0)
+
+//===  strict equality
+console.log(0 == false)
+console.log(0 === false)
