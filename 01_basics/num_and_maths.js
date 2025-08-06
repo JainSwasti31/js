48486dd8771b34ba9dff9c286dd9e3b8141d3aef
JainSwasti31 <148178401+JainSwasti31@users.noreply.github.com>
2025-08-06 10:27:41 +0000

    study

---
+++ 01_basics/num_and_maths.js
@@ -0,0 +1,11 @@
+const score=100
+console.log(score)
+const balance=new Number(100)
+console.log(balance);
+console.log(balance.toString().length);
+// toFixed(3) is used to fix the decimal point
+console.log(balance.toFixed(3)); //it will give output 100.000
+const onum=312.272124
+console.log(onum.toPrecision(4))
+const num=1000000000
+console.log(num.toLocaleString('en-IN'))
---
+++ 01_basics/objects.js
@@ -0,0 +1,9 @@
+// objects literals
+const jsuser={
+    name: "swasti",
+    age: 19,
+    location: "Jaipur",
+    email:"jainswasti3112@gmail.com"
+}
+console.log(jsuser["name"]);
+console.log(jsuser.email);
